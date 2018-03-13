029c7e6fe6e1b3d9b23b66b1f4d09c97ba21663c
emilywestenbroek <em.westenbroek@gmail.com>
2018-03-13 19:16:13 -0400

    First pass at taco-clock service

--- taco-clock/taco-clock.service.js
+++ taco-clock/taco-clock.service.js
@@ -1,3 +1,9 @@
-angular.module("tacoApp").service("TacoClockService", function(){
-  
-});
+angular.module("tacoApp").service("TacoClockService", ["$http", function($http){
+
+
+  this.fetchRestaurants = function() {
+    return $http.get("https://developers.zomato.com/api/v2.1/search?entity_id=817&entity_type=city&cuisines=73&sort=rating&order=desc").then(
+      response => response.data
+    )
+  }
+}]);
