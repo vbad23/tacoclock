db8a9f43c17bc1e2406bca626a921a9db1ad0a89
breetanner <bree@studio-us.com>
2018-03-14 11:48:35 -0400

    Spelled restaurant wrong 4 times... fixed

--- taco-clock/taco-clock.component.js
+++ taco-clock/taco-clock.component.js
@@ -1,9 +1,9 @@
 angular.module("tacoApp").component("tacoClock", {
   templateUrl: "taco-clock/taco-clock.html",
   controller: ['TacoClockService', function(TacoClockService) {
-    this.tacoRestuarants = [];
-    TacoClockService.fetchTacos().then(
-      tacoRestuarants => this.tacoRestuarants = tacoRestuarants // check names
+    this.tacoRestaurants = [];
+    TacoClockService.fetchRestaurants().then(
+      tacoRestaurants => this.tacoRestaurants = tacoRestaurants
     )
   }]
 });
