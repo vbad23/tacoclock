dd4c9a339fcc255740b1ea65390badaa3a322bea
breetanner <bree@studio-us.com>
2018-03-14 11:20:42 -0400

    First commit of Taco Component Code

--- taco-clock/taco-clock.component.js
+++ taco-clock/taco-clock.component.js
@@ -1,6 +1,9 @@
 angular.module("tacoApp").component("tacoClock", {
   templateUrl: "taco-clock/taco-clock.html",
   controller: ['TacoClockService', function(TacoClockService) {
-
+    this.tacoRestuarants = [];
+    TacoClockService.fetchTacos().then(
+      tacoRestuarants => this.tacoRestuarants = tacoRestuarants // check names
+    )
   }]
 });
