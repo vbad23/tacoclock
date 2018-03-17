133d714c10d6611e9ce3f0c4081c4228769ea958
Vikram Badarinath <vbad23@gmail.com>
2018-03-17 10:57:14 -0400

    taco page navigates properly and displays the proper restaurant info

--- taco-restaurant/restaurant.service.js
+++ taco-restaurant/restaurant.service.js
@@ -1,12 +1,23 @@
 angular.module("tacoApp").service("RestaurantService", ["$http", function($http){
 
   this.fetchRestaurants = function() {
-    return $http.get("https://developers.zomato.com/api/v2.1/search?count=20&lat=42.952141211765&lon=-85.640864464171&cuisines=73&sort=real_distance&order=asc",
+    var url = "https://developers.zomato.com/api/v2.1/search?count=20&lat=42.952141211765&lon=-85.640864464171&cuisines=73&sort=real_distance&order=asc"
+    return $http.get(url,
   {headers: {"user-key": 'c3dbbbcffd5c0a6b2d67803c3dcfa4d3'}})
   .then(
       response => response.data.restaurants
     )
   }
+
+  this.fetchRestaurant = function(id) {
+    var url = "https://developers.zomato.com/api/v2.1/restaurant?res_id=" + id;
+    return $http.get(url,
+  {headers: {"user-key": 'c3dbbbcffd5c0a6b2d67803c3dcfa4d3'}})
+  .then(
+      response => response.data
+    )
+  }
+
 }]);
 
 // rename to Restaurant service
