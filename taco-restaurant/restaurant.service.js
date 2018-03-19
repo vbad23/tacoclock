3dbd64fadb53824dc5e097b4625cc967dc82cad9
Vikram Badarinath <vbad23@gmail.com>
2018-03-18 20:15:13 -0400

    Code cleanup. Added text showing search address. Styled restaurant bg color. Added room to the bottom of the list page. Labeled functions.

--- taco-restaurant/restaurant.service.js
+++ taco-restaurant/restaurant.service.js
@@ -1,33 +1,32 @@
 angular.module("tacoApp").service("RestaurantService", ["$http", function($http){
 
+  //API Keys
+  const zomatoKey = "c3dbbbcffd5c0a6b2d67803c3dcfa4d3";
+  const googleGeoKey = "AIzaSyAhXiwRThIxLfgU117A3Z-4g-zKdILrHEc";
+
   // Grand Rapids city center. Shows this by default.
   this.latitude = 42.952141211765;
   this.longitude = -85.640864464171;
+  this.locationName = "Grand Rapids, MI, USA";
 
+  // Gets restaurant list
   this.fetchRestaurants = function() {
     var url = "https://developers.zomato.com/api/v2.1/search?count=20&lat=" + this.latitude + "&lon=" + this.longitude + "&cuisines=73&sort=real_distance&order=asc";
-    return $http.get(url,
-  {headers: {"user-key": 'c3dbbbcffd5c0a6b2d67803c3dcfa4d3'}})
-  .then(
-      response => response.data.restaurants
-    )
+    return $http.get(url, {headers: {"user-key": zomatoKey}})
+  .then(response => response.data.restaurants);
   }
 
+  // Gets a specific restaurant's information
   this.fetchRestaurant = function(id) {
     var url = "https://developers.zomato.com/api/v2.1/restaurant?res_id=" + id;
-    return $http.get(url,
-  {headers: {"user-key": 'c3dbbbcffd5c0a6b2d67803c3dcfa4d3'}})
-  .then(
-      response => response.data
-    )
+    return $http.get(url, {headers: {"user-key": zomatoKey}})
+  .then(response => response.data);
   }
 
+  // Gets location data
   this.fetchCoordinates = function(location) {
-    var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyAhXiwRThIxLfgU117A3Z-4g-zKdILrHEc";
+    var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=" + googleGeoKey;
     return $http.get(url)
-  .then(
-      response => response.data
-    )
+    .then(response => response.data);
   }
-
 }]);
--- taco-restaurant/taco-restaurant.component.js
+++ taco-restaurant/taco-restaurant.component.js
@@ -1,9 +1,8 @@
 angular.module("tacoApp").component("tacoRestaurant", {
   templateUrl: "taco-restaurant/taco-restaurant.html",
   controller: ['RestaurantService', "$routeParams", function(RestaurantService, $routeParams) {
-    console.log($routeParams);
-    // this.priceSymbol = "";
 
+    // Uses the page route to get the restaurant information 
     RestaurantService.fetchRestaurant($routeParams.id).then(
       restaurant => {
         this.restaurant = restaurant;
