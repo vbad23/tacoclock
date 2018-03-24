0cf1c7777344995bda6f0cbe17ca03931b50bf27
breetanner <bree@studio-us.com>
2018-03-24 09:13:56 -0400

    Missing a }

--- taco-restaurant/taco-restaurant.component.js
+++ taco-restaurant/taco-restaurant.component.js
@@ -9,10 +9,11 @@ angular.module("tacoApp").component("tacoRestaurant", {
     RestaurantService.fetchRestaurant($routeParams.id).then(
       restaurant => {
         this.restaurant = restaurant;
-        
+
         this.ratingPercentage = function() {
           return this.restaurant.user_rating.aggregate_rating / 5 * 100;
-          
+        }
+
         this.address = this.restaurant.location.address.split(",");
         if (this.address.length > 2) {
           this.address.push(" ");
